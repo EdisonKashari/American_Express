064b5a170cf79f9c3c9e74dce0cd430a3051ae1d
Edison Kashari <edisonkashari@hotmail.com>
2022-07-13 16:29:05 -0400

    Initial commit

---
+++ src/test/java/com/bank/Amex.java
@@ -0,0 +1,83 @@
+package com.bank;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Amex {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.americanexpress.com/us/credit-cards/?oneXPForcedExperiments=e30=");
+
+
+    }
+/*
+    @Test
+    public void test1() throws InterruptedException {
+
+        WebElement addCart = driver.findElement(By.xpath("(//div[.='Add Card'])[1]"));
+        addCart.click();
+Thread.sleep(2);
+        WebElement businessPlatinium = driver.findElement(By.xpath("//span[contains(text(),'Business Platinum Card')]"));
+
+        businessPlatinium.click();
+
+        WebElement compare = driver.findElement(By.xpath("//button[.='Compare']"));
+
+        compare.click();
+
+        WebElement tab = driver.findElement(By.xpath("//span[contains(text(),'Offer & Benefit Terms')]"));
+        tab.click();
+        String windoowHandles = driver.getWindowHandle();
+        driver.switchTo().window(windoowHandles);
+
+      String actual =   driver.getTitle();
+    }
+
+
+ */
+
+    @Test
+    public void test() throws InterruptedException {
+
+
+        // List<WebElement> allModules = driver.findElements(By.xpath("//button[@data-qe-id='Tab']"));
+
+        WebElement allCards = driver.findElement(By.xpath("//*[@id=\"view-all-personal-cards\"]/div[3]/div[2]/div/ul/li[1]/div/button"));
+        Thread.sleep(2000);
+
+        allCards.click();
+
+       /* for (WebElement allModule : allModules) {
+            System.out.println(allModule.getSize());
+            //  allModule.click();
+            Thread.sleep(3000);
+
+
+        }
+
+
+        */
+
+        //  for(int i = 0;i <allModules.size();i++) {
+
+
+        //  }
+
+
+    }
+
+
+}
